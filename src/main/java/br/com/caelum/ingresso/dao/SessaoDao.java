da26fa2daba3c6cd2be0d0ea8ba89b9ae8590892
Ivana <ivanagv@gmail.com>
2018-11-10 17:08:44 -0200

    Exercio 2.11

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -23,6 +23,8 @@ public class SessaoDao {
 	public List<Sessao>buscaSessoesDaSala(Sala sala){
 		return manager.createQuery("select s from Sessao s where s.sala = :sala", Sessao.class).setParameter("sala", sala).getResultList();
 	}
+	
+	
 	}
 	
 	
